81befa990eb4d55f214d551315b8388f05e18077
Jonas Richard Keller <jonas.r.keller@gmail.com>
2022-08-29 18:20:48 +0200

    Only test 8-bit versions of masked hadd, hadds, hsub and hsubs when compiling for AVX-512

--- simdvecautotestM.C
+++ simdvecautotestM.C
@@ -112,6 +112,7 @@ int main(int argc, char *argv[]) {
   TestListTI<TesterMaskZUnaryTIMinToMax, SW, MaskZ_slli, SIMDWord, SIMDShort,
              SIMDInt>::test(repeats1, pattern);
 
+#ifdef _SIMD_VEC_64_AVAIL_
   TestAll<TesterMaskBinary, SW, Mask_hadd>::test(repeats1, pattern);
   TestAll<TesterMaskZBinary, SW, MaskZ_hadd>::test(repeats1, pattern);
   TestAll<TesterMaskBinary, SW, Mask_hadds>::test(repeats1, pattern);
@@ -120,6 +121,24 @@ int main(int argc, char *argv[]) {
   TestAll<TesterMaskZBinary, SW, MaskZ_hsub>::test(repeats1, pattern);
   TestAll<TesterMaskBinary, SW, Mask_hsubs>::test(repeats1, pattern);
   TestAll<TesterMaskZBinary, SW, MaskZ_hsubs>::test(repeats1, pattern);
+#else
+  TestList<TesterMaskBinary, SW, Mask_hadd, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskZBinary, SW, MaskZ_hadd, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskBinary, SW, Mask_hadds, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskZBinary, SW, MaskZ_hadds, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskBinary, SW, Mask_hsub, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskZBinary, SW, MaskZ_hsub, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskBinary, SW, Mask_hsubs, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+  TestList<TesterMaskZBinary, SW, MaskZ_hsubs, SIMDWord, SIMDShort, SIMDInt,
+           SIMDFloat>::test(repeats1, pattern);
+#endif
 
   TestAll<TesterMaskCmp, SW, Mask_cmplt>::test(repeats1, pattern);
   TestAll<TesterMaskCmp, SW, Mask_cmple>::test(repeats1, pattern);

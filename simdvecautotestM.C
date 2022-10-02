e3e484b7e23dc3eaf4d790fee9847143e99716fb
Jonas Richard Keller <jonas.r.keller@gmail.com>
2022-10-02 15:37:44 +0200

    Apply upstream changes

--- simdvecautotestM.C
+++ simdvecautotestM.C
@@ -1,3 +1,28 @@
+// ===========================================================================
+// 
+// simdvecautotestM.H --
+// autotest for masked operations, Jonas Keller
+// 
+// This source code file is part of the following software:
+// 
+//    - the low-level C++ template SIMD library
+//    - the SIMD implementation of the MinWarping and the 2D-Warping methods 
+//      for local visual homing.
+// 
+// The software is provided based on the accompanying license agreement
+// in the file LICENSE or LICENSE.doc. The software is provided "as is"
+// without any warranty by the licensor and without any liability of the
+// licensor, and the software may not be distributed by the licensee; see
+// the license agreement for details.
+// 
+// (C) Ralf Möller
+//     Computer Engineering
+//     Faculty of Technology
+//     Bielefeld University
+//     www.ti.uni-bielefeld.de
+// 
+// ===========================================================================
+
 #include "SIMDMaskVecAutoTest.H"
 #include "SIMDMaskVecWrapper.H"
 #include "SIMDTypes.H"

bb983d21d4c53b8df89337365080416955f07a0c
Jonas Keller <jonas.r.keller@gmail.com>
2023-11-23 18:47:27 +0100

    Add tests for arm neon to autotest.py

--- autotest.py
+++ autotest.py
@@ -6,17 +6,104 @@ import random
 import time
 import sys
 
-SDE_PATH = ""
-
-COMPILERS = [
-    "clang++",
-    "g++",
-]
+SDE_PATH = "sde64"
 
 SDE_OPTIONS = "-align_checker_action ignore -future"
 
 LOG_DIR = f"autotest_{os.popen('hostname').read().strip()}_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
 TMP_BUILD_DIR = f"/tmp/T-SIMD_autotest_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
+
+
+# modify this function to configure the tests to run
+def generate_test_configs():
+    def get_sandbox_config(compiler):
+        return {
+            "compiler": compiler,
+            "opt_flags": "-O0",
+            "arch_flags": "",
+            "sandbox_defines": "-DSIMDVEC_SANDBOX",
+            "emulator": "",
+        }
+
+    test_configs = []
+
+    opt_flags_list = [
+        # "-O0", compiling with -O0 takes an unreasonable amount ram (like up to 70 GB (wtf?))
+        # "-O1",
+        "-O2",
+        # "-O3",
+        "-O3 -funroll-loops",
+    ]
+    # configs for Intel architectures
+    for compiler in [
+        # "clang++",
+        # "g++",
+    ]:
+        test_configs.append(get_sandbox_config(compiler))
+
+        for arch_flags in [
+            "-msse2",
+            "-msse3",
+            "-mssse3",
+            "-msse4",
+            "-msse4.1",
+            "-msse4.2",
+            "-mavx",
+            "-mavx2",
+            "-mavx512f",
+            "-mavx512bw",
+            "-mavx512dq",
+            "-mavx512vbmi",
+            "-mavx512bw -mavx512dq",
+            "-mavx512dq -mavx512vbmi",
+            # TODO: maybe add more combinations?
+        ]:
+            for opt_flags in opt_flags_list:
+                test_configs.append(
+                    {
+                        "compiler": compiler,
+                        "opt_flags": opt_flags,
+                        "arch_flags": arch_flags,
+                        "emulator": get_required_emulator_intel(arch_flags),
+                    }
+                )
+
+    # configs for ARMv7 architectures
+    for compiler in ["arm-linux-gnueabihf-g++"]:
+        test_configs.append(get_sandbox_config(compiler))
+
+        for arch_flags in [
+            "-march=armv7",
+        ]:
+            for opt_flags in opt_flags_list:
+                test_configs.append(
+                    {
+                        "compiler": compiler,
+                        "opt_flags": opt_flags,
+                        "arch_flags": arch_flags + " -mfpu=neon",
+                        "emulator": "qemu-arm -L /usr/arm-linux-gnueabihf/",
+                    }
+                )
+
+    # configs for ARMv8 architectures
+    for compiler in ["aarch64-linux-gnu-g++"]:
+        test_configs.append(get_sandbox_config(compiler))
+
+        for arch_flags in [
+            "-march=armv8-a",
+        ]:
+            for opt_flags in opt_flags_list:
+                test_configs.append(
+                    {
+                        "compiler": compiler,
+                        "opt_flags": opt_flags,
+                        "arch_flags": arch_flags,
+                        "emulator": "qemu-aarch64",
+                        "static_bins": True,
+                    }
+                )
+
+    return test_configs
 
 
 def is_feature_supported(supported_cpu_features, feature):
@@ -31,6 +118,7 @@ def is_feature_supported(supported_cpu_features, feature):
 
     return False
 
+
 def get_supported_cpu_features():
     supported_cpu_features = []
     with open("/proc/cpuinfo", "r") as f:
@@ -40,11 +128,19 @@ def get_supported_cpu_features():
                 break
 
     for i in range(len(supported_cpu_features)):
-        supported_cpu_features[i] = supported_cpu_features[i].strip().lower().replace("_", "").replace("-", "").replace(".", "")
+        supported_cpu_features[i] = (
+            supported_cpu_features[i]
+            .strip()
+            .lower()
+            .replace("_", "")
+            .replace("-", "")
+            .replace(".", "")
+        )
 
     return supported_cpu_features
 
-def get_required_emulator(arch_flags):
+
+def get_required_emulator_intel(arch_flags):
     SDE_EMULATOR = f"{SDE_PATH} {SDE_OPTIONS} -- "
 
     supported_cpu_features = get_supported_cpu_features()
@@ -54,7 +150,12 @@ def get_required_emulator(arch_flags):
         if arch_flag.startswith("-m"):
             req_cpu_features.append(arch_flag[2:])
 
-    if all([is_feature_supported(supported_cpu_features, req_cpu_feature) for req_cpu_feature in req_cpu_features]):
+    if all(
+        [
+            is_feature_supported(supported_cpu_features, req_cpu_feature)
+            for req_cpu_feature in req_cpu_features
+        ]
+    ):
         # all required cpu features are supported
         # so we don't need to use the emulator
         return ""
@@ -64,55 +165,29 @@ def get_required_emulator(arch_flags):
         return SDE_EMULATOR
 
 
-def generate_test_configs():
-    test_configs = []
-
-    for compiler in COMPILERS:
+def filter_test_configs_for_existing_compilers(test_configs):
+    def check_that_compiler_exists(compiler):
         if os.system(f"which {compiler} > /dev/null 2>&1") != 0:
-            print(f"WARNING: compiler \"{compiler}\" not found, skipping")
-            continue
+            print(
+                f'WARNING: compiler "{compiler}" not found, continue without running tests that use this compiler? (y/n) ',
+                end="",
+            )
+            if input().lower() != "y":
+                print("Aborting.")
+                sys.exit(1)
+            return False
+        return True
 
-        test_configs.append({
-            "compiler": compiler,
-            "opt_flags": "-O0",
-            "arch_flags": "",
-            "sandbox_defines": "-DSIMDVEC_SANDBOX",
-            "emulator": "",
-        })
+    # collect all compilers
+    compilers = set([test_config["compiler"] for test_config in test_configs])
 
-        for opt_flags in [
-            # "-O0", compiling with -O0 takes an unreasonable amount ram (like up to 70 GB (wtf?))
-            #"-O1",
-            "-O2",
-            #"-O3",
-            "-O3 -funroll-loops",
-        ]:
-            for arch_flags in [
-                "-msse2",
-                "-msse3",
-                "-mssse3",
-                "-msse4",
-                "-msse4.1",
-                "-msse4.2",
-                "-mavx",
-                "-mavx2",
-                "-mavx512f",
-                "-mavx512bw",
-                "-mavx512dq",
-                "-mavx512vbmi",
-                "-mavx512bw -mavx512dq",
-                "-mavx512dq -mavx512vbmi",
-                # TODO: maybe add more combinations?
-            ]:
-                test_configs.append({
-                    "compiler": compiler,
-                    "opt_flags": opt_flags,
-                    "arch_flags": arch_flags,
-                    "sandbox_defines": "",
-                    "emulator": get_required_emulator(arch_flags),
-                })
+    # filter out compilers that don't exist
+    compilers = set(filter(check_that_compiler_exists, compilers))
 
-    return test_configs
+    # filter out test configs that use a compiler that doesn't exist
+    return list(
+        filter(lambda test_config: test_config["compiler"] in compilers, test_configs)
+    )
 
 
 def run_test(test_config):
@@ -126,27 +201,31 @@ def run_test(test_config):
     if not os.path.exists(build_dir):
         os.makedirs(build_dir)
 
-    build_command = f"make build_dir=\"{build_dir}\" compiler=\"{test_config['compiler']}\" optflags=\"{test_config['opt_flags']}\" flags_arch=\"{test_config['arch_flags']}\" sandbox_defines=\"{test_config['sandbox_defines']}\" all_tsimd autotest"
+    static_bins_flag = "static_bins=1" if test_config.get("static_bins") else ""
+    build_command = f"make build_dir=\"{build_dir}\" compiler=\"{test_config['compiler']}\" optflags=\"{test_config['opt_flags']}\" flags_arch=\"{test_config['arch_flags']}\" sandbox_defines=\"{test_config['sandbox_defines']}\" {static_bins_flag} all_tsimd autotest"
 
     log_file_compile = f"{LOG_DIR}/{name}_compile.log"
-    os.system(
-        f"make build_dir=\"{build_dir}\" clean > {log_file_compile} 2>&1")
+    os.system(f'make build_dir="{build_dir}" clean > {log_file_compile} 2>&1')
 
     os.system(f"{build_command} >> {log_file_compile} 2>&1")
 
     if test_config["sandbox_defines"] == "":
         log_file_test0 = f"{LOG_DIR}/{name}_test0.log"
         os.system(
-            f"{test_config['emulator']} {build_dir}/simdvecautotest0 \"\" 10000 > {log_file_test0} 2>&1 || echo \"ERROR: simdvecautotest0 failed\" >> {log_file_test0}")
+            f"{test_config['emulator']} {build_dir}/simdvecautotest0 \"\" 10000 > {log_file_test0} 2>&1 || echo \"ERROR: simdvecautotest0 failed\" >> {log_file_test0}"
+        )
         log_file_test1 = f"{LOG_DIR}/{name}_test1.log"
         os.system(
-            f"{test_config['emulator']} {build_dir}/simdvecautotest1 \"\" 10000 > {log_file_test1} 2>&1 || echo \"ERROR: simdvecautotest1 failed\" >> {log_file_test1}")
+            f"{test_config['emulator']} {build_dir}/simdvecautotest1 \"\" 10000 > {log_file_test1} 2>&1 || echo \"ERROR: simdvecautotest1 failed\" >> {log_file_test1}"
+        )
         log_file_testM = f"{LOG_DIR}/{name}_testM.log"
         os.system(
-            f"{test_config['emulator']} {build_dir}/simdvecautotestM \"\" 10000 > {log_file_testM} 2>&1 || echo \"ERROR: simdvecautotestM failed\" >> {log_file_testM}")
+            f"{test_config['emulator']} {build_dir}/simdvecautotestM \"\" 10000 > {log_file_testM} 2>&1 || echo \"ERROR: simdvecautotestM failed\" >> {log_file_testM}"
+        )
         log_file_test_load_store = f"{LOG_DIR}/{name}_test_load_store.log"
         os.system(
-            f"{test_config['emulator']} {build_dir}/simdvecautotest_load_store 10000 > {log_file_test_load_store} 2>&1 || echo \"ERROR: simdvecautotest_load_store failed\" >> {log_file_test_load_store}")
+            f"{test_config['emulator']} {build_dir}/simdvecautotest_load_store 10000 > {log_file_test_load_store} 2>&1 || echo \"ERROR: simdvecautotest_load_store failed\" >> {log_file_test_load_store}"
+        )
 
     os.system(f"rm -r {build_dir}")
 
@@ -169,19 +248,31 @@ if __name__ == "__main__":
             is_sde_path_in_args = True
 
     test_configs = generate_test_configs()
+    test_configs = filter_test_configs_for_existing_compilers(test_configs)
+    for test_config in test_configs:
+        if "sandbox_defines" not in test_config:
+            test_config["sandbox_defines"] = ""
 
-    is_sde_required = any([test_config["emulator"] != "" for test_config in test_configs])
+    is_sde_required = any(
+        [test_config["emulator"].startswith(SDE_PATH) for test_config in test_configs]
+    )
 
-    if is_sde_required and  not os.path.exists(SDE_PATH):
+    if is_sde_required and not os.path.exists(SDE_PATH):
         if is_sde_path_in_args:
             print(
-                f"ERROR: Intel® SDE is required for some tests, but SDE_PATH from command line arguments is invalid: \"{SDE_PATH}\"")
+                f'ERROR: Intel® SDE is required for some tests, but SDE_PATH from command line arguments is invalid: "{SDE_PATH}"'
+            )
         elif is_sde_path_in_env:
             print(
-                f"ERROR: Intel® SDE is required for some tests, but SDE_PATH from environment variables is invalid: \"{SDE_PATH}\"")
+                f'ERROR: Intel® SDE is required for some tests, but SDE_PATH from environment variables is invalid: "{SDE_PATH}"'
+            )
         else:
-            print(f"ERROR: Intel® SDE is required for some tests, but SDE_PATH from constant is invalid: \"{SDE_PATH}\"")
-        print("       Please specify a valid SDE_PATH pointing to the sde or sde64 executable (download from https://www.intel.com/content/www/us/en/developer/articles/tool/software-development-emulator.html) with one of the following options:")
+            print(
+                f'ERROR: Intel® SDE is required for some tests, but SDE_PATH from constant is invalid: "{SDE_PATH}"'
+            )
+        print(
+            "       Please specify a valid SDE_PATH pointing to the sde or sde64 executable (download from https://www.intel.com/content/www/us/en/developer/articles/tool/software-development-emulator.html) with one of the following options:"
+        )
         print("       - with the --sde-path command line argument")
         print("       - with the SDE_PATH= command line argument")
         print("       - with the SDE_PATH environment variable")
@@ -201,8 +292,11 @@ if __name__ == "__main__":
     num_procs_ram = ram_size_kb // (15 * 1024 * 1024)
 
     num_procs = min(num_procs_cores, num_procs_ram)
+    num_procs = 8  # TODO: remove this line
 
-    print(f"Using {num_procs} processes ({num_procs_cores} based on number of cores, {num_procs_ram} based on RAM size)")
+    print(
+        f"Using {num_procs} processes ({num_procs_cores} based on number of cores, {num_procs_ram} based on RAM size)"
+    )
 
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
@@ -225,14 +319,19 @@ if __name__ == "__main__":
     pool.join()
 
     os.system(
-        f"grep -nri --exclude=errors.log --exclude=warnings.log \"error\" {LOG_DIR} > {LOG_DIR}/errors.log")
+        f'grep -nri --exclude=errors.log --exclude=warnings.log "error" {LOG_DIR} > {LOG_DIR}/errors.log'
+    )
     os.system(
-        f"grep -nri --exclude=errors.log --exclude=warnings.log \"warning\" {LOG_DIR} > {LOG_DIR}/warnings.log")
+        f'grep -nri --exclude=errors.log --exclude=warnings.log "warning" {LOG_DIR} > {LOG_DIR}/warnings.log'
+    )
 
     os.system(
-        f"echo \"Number of errors (see file {LOG_DIR}/errors.log):\" && cat {LOG_DIR}/errors.log | wc -l")
+        f'echo "Number of errors (see file {LOG_DIR}/errors.log):" && cat {LOG_DIR}/errors.log | wc -l'
+    )
     os.system(
-        f"echo \"Number of warnings (see file {LOG_DIR}/warnings.log): \" && cat {LOG_DIR}/warnings.log | wc -l")
+        f'echo "Number of warnings (see file {LOG_DIR}/warnings.log): " && cat {LOG_DIR}/warnings.log | wc -l'
+    )
 
     print(
-        f"Done. Executed {len(test_configs)} tests in {time.time() - start_time} seconds")
+        f"Done. Executed {len(test_configs)} tests in {time.time() - start_time} seconds"
+    )

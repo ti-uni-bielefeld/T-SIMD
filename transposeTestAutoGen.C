469092a4b624d8359e700de0e2d754c99763ed06
Jonas Keller <jonas.r.keller@gmail.com>
2024-04-20 12:47:25 +0200

    Autoformat

--- transposeTestAutoGen.C
+++ transposeTestAutoGen.C
@@ -1,53 +1,53 @@
 // ===========================================================================
-// 
+//
 // transposeTestAutoGen.C --
 // test of auto-generated transpose functions
-// 
+//
 // This source code file is part of the following software:
-// 
+//
 //    - the low-level C++ template SIMD library
-//    - the SIMD implementation of the MinWarping and the 2D-Warping methods 
+//    - the SIMD implementation of the MinWarping and the 2D-Warping methods
 //      for local visual homing.
-// 
+//
 // The software is provided based on the accompanying license agreement
 // in the file LICENSE or LICENSE.doc. The software is provided "as is"
 // without any warranty by the licensor and without any liability of the
 // licensor, and the software may not be distributed by the licensee; see
 // the license agreement for details.
-// 
+//
 // (C) Ralf Möller
 //     Computer Engineering
 //     Faculty of Technology
 //     Bielefeld University
 //     www.ti.uni-bielefeld.de
-// 
+//
 // ===========================================================================
 
 #define MAX_SIMD_WIDTH 64
 
-#include <stdlib.h>
-#include "tsimd.H"
 #include "TimeMeasurement.H"
+#include "tsimd.H"
+
+#include <stdlib.h>
 
 // NOTE: byte may not be enough to fill each element with a unique value
 // for large matrices
 
-//#define TYPE simd::Float
-//#define TFORMAT "%3g "
+// #define TYPE simd::Float
+// #define TFORMAT "%3g "
 
-//#define TYPE simd::Byte
-//#define TFORMAT "%3d "
+// #define TYPE simd::Byte
+// #define TFORMAT "%3d "
 
-#define TYPE simd::Double
+#define TYPE    simd::Double
 #define TFORMAT "%3g "
 
-#define barrier() __asm__ __volatile__("": : :"memory")
+#define barrier() __asm__ __volatile__("" : : : "memory")
 
 // ===========================================================================
 
 template <typename T>
-void
-transposeCheck(simd::Vec<T> *rows1, simd::Vec<T> *rows2)
+void transposeCheck(simd::Vec<T> *rows1, simd::Vec<T> *rows2)
 {
   constexpr int n = simd::Vec<T>::elements;
   T buf1[n * n], buf2[n * n];
@@ -66,74 +66,71 @@ transposeCheck(simd::Vec<T> *rows1, simd::Vec<T> *rows2)
 // ===========================================================================
 
 // also odd number of repeats
-#define TEST_TRANSPOSE(TFUNCNAME)                                \
-  puts("========== " #TFUNCNAME "  ==========");                 \
-  t0 = simd::getTimeSpec();                                      \
-  for (long r = 0; r < (repeats | 1); r++) {                     \
-    barrier();                                                   \
-    TFUNCNAME(inRows, outRows);                                  \
-    barrier();                                                   \
-  }                                                              \
-  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);          \
-  printf("TIME dt = %10.0f for " #TFUNCNAME "\n", dt);           \
-  fflush(stdout);                                                \
-  for (unsigned int i = 0; i < elems; i++) {                     \
-    printf("outRows[%2d] = ", i);                                \
-    print(TFORMAT, outRows[i]);                                  \
-    printf("\n");                                                \
-  }                                                              \
-  puts("");                                                      \
+#define TEST_TRANSPOSE(TFUNCNAME)                                              \
+  puts("========== " #TFUNCNAME "  ==========");                               \
+  t0 = simd::getTimeSpec();                                                    \
+  for (long r = 0; r < (repeats | 1); r++) {                                   \
+    barrier();                                                                 \
+    TFUNCNAME(inRows, outRows);                                                \
+    barrier();                                                                 \
+  }                                                                            \
+  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);                        \
+  printf("TIME dt = %10.0f for " #TFUNCNAME "\n", dt);                         \
+  fflush(stdout);                                                              \
+  for (unsigned int i = 0; i < elems; i++) {                                   \
+    printf("outRows[%2d] = ", i);                                              \
+    print(TFORMAT, outRows[i]);                                                \
+    printf("\n");                                                              \
+  }                                                                            \
+  puts("");                                                                    \
   transposeCheck(inRows, outRows);
 
 // test of single-arg versions
 // odd number of repeats
-#define TEST_TRANSPOSE_1ARG(TFUNCNAME)                           \
-  puts("========== " #TFUNCNAME "  ==========");                 \
-  for (int i = 0; i < elems; i++)                                \
-    rows[i] = inRows[i];                                         \
-  t0 = simd::getTimeSpec();                                      \
-  for (long r = 0; r < (repeats | 1); r++) {                     \
-    barrier();                                                   \
-    TFUNCNAME(rows);                                             \
-    barrier();                                                   \
-  }                                                              \
-  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);          \
-  printf("TIME dt = %10.0f for " #TFUNCNAME " (1 arg)\n", dt);   \
-  fflush(stdout);                                                \
-  for (unsigned int i = 0; i < elems; i++) {                     \
-    printf("rows[%2d] = ", i);                                   \
-    print(TFORMAT, rows[i]);                                     \
-    printf("\n");                                                \
-  }                                                              \
-  puts("");                                                      \
+#define TEST_TRANSPOSE_1ARG(TFUNCNAME)                                         \
+  puts("========== " #TFUNCNAME "  ==========");                               \
+  for (int i = 0; i < elems; i++) rows[i] = inRows[i];                         \
+  t0 = simd::getTimeSpec();                                                    \
+  for (long r = 0; r < (repeats | 1); r++) {                                   \
+    barrier();                                                                 \
+    TFUNCNAME(rows);                                                           \
+    barrier();                                                                 \
+  }                                                                            \
+  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);                        \
+  printf("TIME dt = %10.0f for " #TFUNCNAME " (1 arg)\n", dt);                 \
+  fflush(stdout);                                                              \
+  for (unsigned int i = 0; i < elems; i++) {                                   \
+    printf("rows[%2d] = ", i);                                                 \
+    print(TFORMAT, rows[i]);                                                   \
+    printf("\n");                                                              \
+  }                                                                            \
+  puts("");                                                                    \
   transposeCheck(inRows, rows);
 
 // test of single-arg versions, emulated with two-arg versions
 // odd number of repeats
 // use only for auto-generated inplc functions
-#define TEST_TRANSPOSE_1ARG_EMU(TFUNCNAME)                              \
-  puts("========== " #TFUNCNAME "  ==========");                        \
-  for (int i = 0; i < elems; i++)                                       \
-    rows[i] = inRows[i];                                                \
-  t0 = simd::getTimeSpec();                                             \
-  for (long r = 0; r < (repeats | 1); r++) {                            \
-    barrier();                                                          \
-    TFUNCNAME(rows, rows);                                              \
-    barrier();                                                          \
-  }                                                                     \
-  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);                 \
-  printf("TIME dt = %10.0f for " #TFUNCNAME " (1 arg, emu)\n", dt);     \
-  fflush(stdout);                                                       \
-  for (unsigned int i = 0; i < elems; i++) {                            \
-    printf("rows[%2d] = ", i);                                          \
-    print(TFORMAT, rows[i]);                                            \
-    printf("\n");                                                       \
-  }                                                                     \
-  puts("");                                                             \
+#define TEST_TRANSPOSE_1ARG_EMU(TFUNCNAME)                                     \
+  puts("========== " #TFUNCNAME "  ==========");                               \
+  for (int i = 0; i < elems; i++) rows[i] = inRows[i];                         \
+  t0 = simd::getTimeSpec();                                                    \
+  for (long r = 0; r < (repeats | 1); r++) {                                   \
+    barrier();                                                                 \
+    TFUNCNAME(rows, rows);                                                     \
+    barrier();                                                                 \
+  }                                                                            \
+  dt = simd::timeSpecDiffUsec(simd::getTimeSpec(), t0);                        \
+  printf("TIME dt = %10.0f for " #TFUNCNAME " (1 arg, emu)\n", dt);            \
+  fflush(stdout);                                                              \
+  for (unsigned int i = 0; i < elems; i++) {                                   \
+    printf("rows[%2d] = ", i);                                                 \
+    print(TFORMAT, rows[i]);                                                   \
+    printf("\n");                                                              \
+  }                                                                            \
+  puts("");                                                                    \
   transposeCheck(inRows, rows);
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   if (argc != 2) {
     fprintf(stderr, "usage: transposeTest <repeats>\n");
@@ -141,13 +138,13 @@ main(int argc, char *argv[])
   }
   long repeats = atol(argv[1]);
   printf("repeats = %ld\n", repeats);
-  
-  constexpr int elems = simd::Vec<TYPE>::elements; 
+
+  constexpr int elems = simd::Vec<TYPE>::elements;
   simd::Vec<TYPE> inRows[elems], outRows[elems], rows[elems];
   TYPE buf[elems * elems];
   struct timespec t0;
   double dt;
-  
+
   for (unsigned int i = 0; i < elems * elems; i++) buf[i] = TYPE(i);
   for (unsigned int i = 0; i < elems; i++) {
     inRows[i] = simd::loadu(buf + i * elems);
@@ -166,7 +163,7 @@ main(int argc, char *argv[])
   TEST_TRANSPOSE(simd::internal::ext::transpose_g);
   TEST_TRANSPOSE(simd::internal::ext::transpose_h);
   TEST_TRANSPOSE(simd::internal::ext::transpose_i);
-  
+
   TEST_TRANSPOSE(simd::internal::ext::transpose1inplc);
   TEST_TRANSPOSE(simd::internal::ext::transpose1inplcLane);
   TEST_TRANSPOSE(simd::internal::ext::transpose2inplc);
--- verticalBitonicSortTest.C
+++ verticalBitonicSortTest.C
@@ -1,26 +1,26 @@
 // ===========================================================================
-// 
+//
 // verticalBitonicSortTest.C --
 // test of verticalBitonicSort function
-// 
+//
 // This source code file is part of the following software:
-// 
+//
 //    - the low-level C++ template SIMD library
-//    - the SIMD implementation of the MinWarping and the 2D-Warping methods 
+//    - the SIMD implementation of the MinWarping and the 2D-Warping methods
 //      for local visual homing.
-// 
+//
 // The software is provided based on the accompanying license agreement
 // in the file LICENSE or LICENSE.doc. The software is provided "as is"
 // without any warranty by the licensor and without any liability of the
 // licensor, and the software may not be distributed by the licensee; see
 // the license agreement for details.
-// 
+//
 // (C) Ralf Möller
 //     Computer Engineering
 //     Faculty of Technology
 //     Bielefeld University
 //     www.ti.uni-bielefeld.de
-// 
+//
 // ===========================================================================
 
 // contributed by Ebba Stina Siebold, modified
@@ -33,187 +33,180 @@
 const int SIMD_WIDTH = 64;
 // iterations 10000, 1000000
 const int ITERATIONS_VALIDATION = 1000;
-const int ITERATIONS = 100000;
+const int ITERATIONS            = 100000;
 //=====================================
 
-namespace SortHelp
-{  
-  template <class T>
-  T* alloc_array(int length)
-  {
-    return (T*) malloc(sizeof(T)*length);
+namespace SortHelp {
+template <class T>
+T *alloc_array(int length)
+{
+  return (T *) malloc(sizeof(T) * length);
+}
+
+template <class T>
+void random_init_array(T *data, int length)
+{
+  T range_min = std::numeric_limits<T>::min() / 10;
+  T range_max = std::numeric_limits<T>::max() / 10;
+  for (int i = 0; i < length; i++) {
+    data[i] =
+      T(float(rand()) / float(RAND_MAX) * (range_max - range_min) + range_min);
   }
-  
-  template <class T>
-  void random_init_array(T *data, int length)
-  {
-    T range_min = std::numeric_limits<T>::min()/10;
-    T range_max = std::numeric_limits<T>::max()/10;
-    for (int i = 0; i < length; i++) {
-      data[i] = T(float(rand())/float(RAND_MAX) *
-                  (range_max - range_min) + range_min);
-    }
-  }
-  
-  template <class T>
-  void copy_array(T *original, T *copy, int length)
-  {
-    for (int i = 0; i < length; i++) {
-      copy[i] = original[i];
-    }
-  }
-  
-  template <class T>
-  void print_array(T *data, int length)
-  {
+}
+
+template <class T>
+void copy_array(T *original, T *copy, int length)
+{
+  for (int i = 0; i < length; i++) { copy[i] = original[i]; }
+}
+
+template <class T>
+void print_array(T *data, int length)
+{
+  std::cout << "[";
+  for (int i = 0; i < length - 1; i++) { std::cout << data[i] << ", "; }
+  std::cout << data[length - 1] << "]\n";
+}
+
+template <class T, int SIMD_WIDTH>
+void print_vecs(T *data, int length)
+{
+  // number of elements in a simd vector
+  constexpr int SIMD_ELEMS = SIMD_WIDTH / sizeof(T);
+  int i;
+  for (i = 0; i < length / SIMD_ELEMS; i++) {
     std::cout << "[";
-    for(int i = 0; i < length-1; i++) {
+    for (int j = i * SIMD_ELEMS; j < SIMD_ELEMS * (1 + i) - 1; j++) {
+      std::cout << data[j] << ", ";
+    }
+    std::cout << data[SIMD_ELEMS * (i + 1) - 1] << "]\n";
+  }
+  if (i * SIMD_ELEMS != length) {
+    std::cout << "[";
+    for (i = i * SIMD_ELEMS; i < length - 1; i++) {
       std::cout << data[i] << ", ";
     }
-    std::cout << data[length-1] << "]\n";
+    std::cout << data[length - 1] << "]\n";
   }
-  
-  template <class T, int SIMD_WIDTH>
-  void print_vecs(T *data, int length)
-  {
-    // number of elements in a simd vector
-    constexpr int SIMD_ELEMS = SIMD_WIDTH/sizeof(T);
-    int i;
-    for (i = 0; i < length/SIMD_ELEMS; i++) {
-      std::cout << "[";
-      for (int j = i*SIMD_ELEMS; j < SIMD_ELEMS*(1+i) - 1; j++) {
-        std::cout << data[j] << ", ";
-      }
-      std::cout << data[SIMD_ELEMS*(i+1)-1] << "]\n";
-    }
-    if (i*SIMD_ELEMS != length) {   
-      std::cout << "[";
-      for (i = i*SIMD_ELEMS; i < length -1; i++) {
-        std::cout << data[i] << ", ";
-      }
-      std::cout << data[length-1] << "]\n";
+}
+
+template <class T, int SIMD_WIDTH>
+void test_equal_array(T *array1, T *array2, int length)
+{
+  for (int i = 0; i < length; i++) {
+    if (array1[i] != array2[i]) {
+      printf("Difference in arrays (n=%d) at index %d.\n", length, i);
+      printf("Array 1:\n");
+      print_vecs<T, SIMD_WIDTH>(array1, length);
+      printf("Array 2:\n");
+      print_vecs<T, SIMD_WIDTH>(array2, length);
+      return;
     }
   }
-  
-  template <class T, int SIMD_WIDTH>
-  void test_equal_array(T *array1, T *array2, int length)
-  {
-    for (int i = 0; i < length; i++) {
-      if (array1[i] != array2[i]) {
-        printf("Difference in arrays (n=%d) at index %d.\n", length, i);
-        printf("Array 1:\n");
-        print_vecs<T, SIMD_WIDTH>(array1, length);
-        printf("Array 2:\n");
-        print_vecs<T, SIMD_WIDTH>(array2, length);
-        return;
-      }
-    }
-  }
-} // namespace
+}
+} // namespace SortHelp
 
 //======= TESTS AND BENCHMARKS ========
 
-template<class T, int LENGTH>
+template <class T, int LENGTH>
 void validate_vertical(int iterations)
 {
-  for(int i = 0; i < iterations; i++) {
-    T* original = SortHelp::alloc_array<T>(LENGTH);
-    T* copy_vertical = SortHelp::alloc_array<T>(LENGTH);
-    T* copy_stdsort = SortHelp::alloc_array<T>(LENGTH);
+  for (int i = 0; i < iterations; i++) {
+    T *original      = SortHelp::alloc_array<T>(LENGTH);
+    T *copy_vertical = SortHelp::alloc_array<T>(LENGTH);
+    T *copy_stdsort  = SortHelp::alloc_array<T>(LENGTH);
     SortHelp::random_init_array<T>(original, LENGTH);
-    
+
     // sort with bitonic sort
     SortHelp::copy_array<T>(original, copy_vertical, LENGTH);
-    simd::verticalBitonicSort
-      <LENGTH, simd::SortSlope::ASCENDING, T, SIMD_WIDTH>(copy_vertical);
-    
+    simd::verticalBitonicSort<LENGTH, simd::SortSlope::ASCENDING, T,
+                              SIMD_WIDTH>(copy_vertical);
+
     // sort with std::sort
     SortHelp::copy_array<T>(original, copy_stdsort, LENGTH);
     std::sort(&copy_stdsort[0], &copy_stdsort[LENGTH]);
-    
-    SortHelp::test_equal_array<T, SIMD_WIDTH>
-      (copy_stdsort, copy_vertical, LENGTH);
-    
+
+    SortHelp::test_equal_array<T, SIMD_WIDTH>(copy_stdsort, copy_vertical,
+                                              LENGTH);
+
     free(original);
     free(copy_vertical);
     free(copy_stdsort);
   }
 }
 
-template<class T>
+template <class T>
 void validate_vertical_all(int iterations)
 {
-  constexpr int SIMD_ELEMS = SIMD_WIDTH/sizeof(T);
-  constexpr int MIN_LENGTH = SIMD_ELEMS*SIMD_ELEMS;
+  constexpr int SIMD_ELEMS = SIMD_WIDTH / sizeof(T);
+  constexpr int MIN_LENGTH = SIMD_ELEMS * SIMD_ELEMS;
   validate_vertical<T, MIN_LENGTH>(iterations);
-  validate_vertical<T, MIN_LENGTH*2>(iterations);
-  validate_vertical<T, MIN_LENGTH*4>(iterations);
-  validate_vertical<T, MIN_LENGTH*8>(iterations);
+  validate_vertical<T, MIN_LENGTH * 2>(iterations);
+  validate_vertical<T, MIN_LENGTH * 4>(iterations);
+  validate_vertical<T, MIN_LENGTH * 8>(iterations);
 }
 
-template<class T, int LENGTH>
+template <class T, int LENGTH>
 void benchmark_vertical(int iterations)
 {
   clock_t time;
   double time_total = 0;
-  T* data = SortHelp::alloc_array<T>(LENGTH);
-  for(int i = 0; i < iterations; i++) {
+  T *data           = SortHelp::alloc_array<T>(LENGTH);
+  for (int i = 0; i < iterations; i++) {
     SortHelp::random_init_array<T>(data, LENGTH);
-    
+
     time = clock();
-    simd::verticalBitonicSort
-      <LENGTH, simd::SortSlope::ASCENDING, T, SIMD_WIDTH>(data);
-    time = clock()-time;
-    time_total += ((double)time)/CLOCKS_PER_SEC;
-    
+    simd::verticalBitonicSort<LENGTH, simd::SortSlope::ASCENDING, T,
+                              SIMD_WIDTH>(data);
+    time = clock() - time;
+    time_total += ((double) time) / CLOCKS_PER_SEC;
   }
   free(data);
   printf("vertical bitonic sort: %fs\n", time_total);
 }
 
-template<class T, int LENGTH>
+template <class T, int LENGTH>
 void benchmark_stdsort(int iterations)
 {
   clock_t time;
   double time_total = 0;
-  T* data = SortHelp::alloc_array<T>(LENGTH);
-  for(int i = 0; i < iterations; i++) {
+  T *data           = SortHelp::alloc_array<T>(LENGTH);
+  for (int i = 0; i < iterations; i++) {
     SortHelp::random_init_array<T>(data, LENGTH);
-    
+
     time = clock();
     std::sort(&data[0], &data[LENGTH]);
-    time = clock()-time;
-    time_total += ((double)time)/CLOCKS_PER_SEC;
+    time = clock() - time;
+    time_total += ((double) time) / CLOCKS_PER_SEC;
   }
   free(data);
   printf("std::sort: %fs\n", time_total);
 }
 
-template<class T>
+template <class T>
 void benchmark_vertical_all(int iterations)
 {
-  constexpr int SIMD_ELEMS = SIMD_WIDTH/sizeof(T);
-  constexpr int MIN_LENGTH = SIMD_ELEMS*SIMD_ELEMS;
+  constexpr int SIMD_ELEMS = SIMD_WIDTH / sizeof(T);
+  constexpr int MIN_LENGTH = SIMD_ELEMS * SIMD_ELEMS;
   benchmark_vertical<T, MIN_LENGTH>(iterations);
-  benchmark_vertical<T, MIN_LENGTH*2>(iterations);
-  benchmark_vertical<T, MIN_LENGTH*4>(iterations);
-  benchmark_vertical<T, MIN_LENGTH*8>(iterations);
+  benchmark_vertical<T, MIN_LENGTH * 2>(iterations);
+  benchmark_vertical<T, MIN_LENGTH * 4>(iterations);
+  benchmark_vertical<T, MIN_LENGTH * 8>(iterations);
 }
 
-template<class T>
+template <class T>
 void benchmark_stdsort_all(int iterations)
 {
-  constexpr int SIMD_ELEMS = SIMD_WIDTH/sizeof(T);
-  constexpr int MIN_LENGTH = SIMD_ELEMS*SIMD_ELEMS;
+  constexpr int SIMD_ELEMS = SIMD_WIDTH / sizeof(T);
+  constexpr int MIN_LENGTH = SIMD_ELEMS * SIMD_ELEMS;
   benchmark_stdsort<T, MIN_LENGTH>(iterations);
-  benchmark_stdsort<T, MIN_LENGTH*2>(iterations);
-  benchmark_stdsort<T, MIN_LENGTH*4>(iterations);
-  benchmark_stdsort<T, MIN_LENGTH*8>(iterations);
+  benchmark_stdsort<T, MIN_LENGTH * 2>(iterations);
+  benchmark_stdsort<T, MIN_LENGTH * 4>(iterations);
+  benchmark_stdsort<T, MIN_LENGTH * 8>(iterations);
 }
 
-//=====================================                                                   
-int
-main(int, char **)
+//=====================================
+int main(int, char **)
 {
   // validation of the vertical bitonic sort function
   constexpr int iterations_validation = ITERATIONS_VALIDATION;
@@ -235,7 +228,7 @@ main(int, char **)
   printf("LONG...\n");
   validate_vertical_all<long>(iterations_validation);
   printf("VALIDATION DONE\n");
-  
+
   // Benchmarks of all possible types and sorting functions
   constexpr int iterations = ITERATIONS;
   printf("\nBENCHMARKS\n");
@@ -256,7 +249,7 @@ main(int, char **)
   benchmark_vertical_all<double>(iterations);
   printf("\nlong\n");
   benchmark_vertical_all<long>(iterations);
-  
+
   printf("\nSTD::SORT\n");
   printf("signed byte\n");
   benchmark_vertical_all<int8_t>(iterations);
@@ -275,4 +268,3 @@ main(int, char **)
   printf("\nlong\n");
   benchmark_vertical_all<long>(iterations);
 }
-
